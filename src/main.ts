24492a0b73a62f8adcc655eab8785607bf5ca1a6
mrs-y-arai <sidvivid9672@gmail.com>
2025-01-29 22:26:10 +0900

    DB設定・Place定義・DB繋がってるかとりあえず実装

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,26 @@
 import express, { Request, Response } from "express";
+import { PrismaClient } from "@prisma/client";
+import { placeListSchema } from "./schema/Place";
+
+const prisma = new PrismaClient();
+
 const app = express();
 const port = 3000;
 
-// APIのエンドポイント
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello World");
+app.get("/", async (req: Request, res: Response) => {
+  const places = await prisma.$queryRaw`
+    SELECT id, name, description,ST_AsGeoJSON(location)::json as location, created_at
+    FROM "places"
+  `;
+  const parsedPlaces = placeListSchema.safeParse(places);
+  if (!parsedPlaces.success) {
+    console.error("Failed to parse places", parsedPlaces.error.format());
+    res.status(500).json({ error: "Failed to parse places" });
+    return;
+  }
+  res.json({ data: parsedPlaces.data });
 });
 
-// サーバー起動
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
