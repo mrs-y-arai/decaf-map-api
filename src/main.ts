14bcb9dc492e0b8067f92b0212632bd0132a36b9
mrs-y-arai <sidvivid9672@gmail.com>
2025-03-02 23:46:59 +0900

    authMiddleware・cors・userRepository

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,20 @@
 import express from 'express';
 import { PlaceRouter } from '~/routes/PlaceRoute';
+import cors from 'cors';
 
 const app = express();
 
 const API_BASE_URL = '/api/v1';
-const port = 3000;
+const port = 8080;
+const frontendBaseUrl = process.env.FRONTEND_BASE_URL;
 
+const corsOptions = {
+  origin: frontendBaseUrl,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  optionsSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(`${API_BASE_URL}/places`, PlaceRouter);
 
--- src/routes/PlaceRoute.ts
+++ src/routes/PlaceRoute.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import { PlaceController } from '~/controllers/PlaceController.js';
+import { authMiddleware } from '~/middleware/authMiddleware.js';
 
 export const PlaceRouter = Router();
 
@@ -8,4 +9,4 @@ const placeController = new PlaceController();
 PlaceRouter.get('/', placeController.getPlaces);
 PlaceRouter.get('/search', placeController.searchNearByPoint);
 PlaceRouter.get('/:id', placeController.getPlace);
-PlaceRouter.post('/', placeController.createPlace);
+PlaceRouter.post('/', authMiddleware, placeController.createPlace);
